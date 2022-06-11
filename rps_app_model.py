f8e791ab0187b757caa8566e8ac85aacc9bfd4cd
liang799 <neohtp@tutanota.com>
2022-06-11 22:49:02 +0800

    Changed rps_app_model to use a custom NN

--- rps_app_model.py
+++ rps_app_model.py
@@ -5,7 +5,6 @@ Created on Thu Jun 25 04:09:59 2020
 @author: ASUS
 """
 
-import keras
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.layers import Flatten, Dense, Dropout
 from tensorflow.keras.models import Model
@@ -44,44 +43,41 @@ def image_gen_w_aug(train_parent_directory, test_parent_directory):
     return train_generator, val_generator, test_generator
 
 
-def model_output_for_TL(pre_trained_model, last_output):
-    x = Flatten()(last_output)
-
-    # Dense hidden layer
-    x = Dense(512, activation='relu')(x)
-    x = Dropout(0.2)(x)
-
-    # Output neuron. 
-    x = Dense(3, activation='softmax')(x)
-
-    model = Model(pre_trained_model.input, x)
-
-    return model
-
 
 train_dir = os.path.join('C:/Python/rps/datasets/train/')
 test_dir = os.path.join('C:/Python/rps/datasets/test/')
 
 train_generator, validation_generator, test_generator = image_gen_w_aug(train_dir, test_dir)
 
-pre_trained_model = InceptionV3(input_shape=(75, 75, 3),
-                                include_top=False,
-                                weights='imagenet')
+model = tf.keras.models.Sequential([
+  # Note the input shape is the desired size of the image:
+  # 150x150 with 3 bytes color
+  # This is the first convolution
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu',
+              input_shape=(75, 75, 3)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  # The second convolution
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  # The third convolution
+  tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  # The fourth convolution
+  tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  # Flatten the results to feed into a DNN
+  tf.keras.layers.Flatten(),
+  # 512 neuron hidden layer
+  tf.keras.layers.Dense(512, activation='relu'),
+  tf.keras.layers.Dense(3, activation='softmax')
+])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-for layer in pre_trained_model.layers:
-    layer.trainable = False
-
-last_layer = pre_trained_model.get_layer('mixed3')
-last_output = last_layer.output
-
-model_TL = model_output_for_TL(pre_trained_model, last_output)
-model_TL.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-history_TL = model_TL.fit(
+history = model.fit(
     train_generator,
     steps_per_epoch=10,
     epochs=5,
     verbose=1,
     validation_data=validation_generator)
 
-tf.keras.models.save_model(model_TL, 'my_model.hdf5')
+tf.keras.models.save_model(model, 'my_model.hdf5')
