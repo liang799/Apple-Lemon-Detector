b976bb7da55e3df9d52e0bf5d2ec0c09ee040ca9
liang799 <neohtp@tutanota.com>
2022-06-16 20:17:23 +0800

    Added earlystopping

--- rps_app_model.py
+++ rps_app_model.py
@@ -1,11 +1,15 @@
-from functions import image_gen_w_aug
+from functions import image_gen_w_aug, plot_confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix
+from keras import callbacks
 import tensorflow as tf
+import numpy as np
 import os
 
-train_dir = os.path.join('C:/Python/rps/datasets/train/')
-test_dir = os.path.join('C:/Python/rps/datasets/test/')
+train_dir = os.path.join('C:/Python/Apple-Lemon-Detector/datasets/train/')
+val_dir = os.path.join('C:/Python/Apple-Lemon-Detector/datasets/val/')
+test_dir = os.path.join('C:/Python/Apple-Lemon-Detector/datasets/test/')
 
-train_generator, validation_generator, test_generator = image_gen_w_aug(train_dir, test_dir)
+train_generator, validation_generator, test_generator = image_gen_w_aug(train_dir, val_dir, test_dir)
 
 model = tf.keras.models.Sequential([
     # input is 75x75 with 3 bytes color
@@ -30,11 +34,29 @@ model = tf.keras.models.Sequential([
 ])
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
+
+earlystopping = callbacks.EarlyStopping(monitor="val_loss",
+                                        mode="min", patience=5,
+                                        restore_best_weights=True)
 history = model.fit(
     train_generator,
-    steps_per_epoch=10,
-    epochs=5,
+    epochs=25,
     verbose=1,
-    validation_data=validation_generator)
+    validation_data=validation_generator,
+    callbacks=earlystopping)
 
-tf.keras.models.save_model(model, 'my_model.hdf5')
+# tf.keras.models.save_model(model, 'my_model.hdf5')
+
+# Get classes
+target_names = []
+for key in train_generator.class_indices:
+    target_names.append(key)
+
+# Plot Confusion Matrix
+Y_pred = model.predict(test_generator)
+y_pred = np.argmax(Y_pred, axis=1)
+cm = confusion_matrix(test_generator.classes, y_pred)
+plot_confusion_matrix(cm, target_names, title='Confusion Matrix')
+
+# Print Classification Report
+print(classification_report(test_generator.classes, y_pred, target_names=target_names))
