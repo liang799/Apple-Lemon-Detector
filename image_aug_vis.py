aa40565642e0e4146d895b88827793c5052fc134
liang799 <neohtp@tutanota.com>
2022-06-19 20:50:35 +0800

    Added augmentation visualisation

---
+++ image_aug_vis.py
@@ -0,0 +1,28 @@
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+image_path = 'C://Python//Apple-Lemon-Detector//datasets//train//lemon//1 (212).jpg'
+
+# Loads image in from the set image path
+img = keras.preprocessing.image.load_img(image_path, target_size=(75, 75))
+img_tensor = keras.preprocessing.image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+# Uses ImageDataGenerator to flip the images
+datagen = ImageDataGenerator(#rescale=1. / 255,
+                             rotation_range=30,
+                             zoom_range=0.2,
+                             width_shift_range=0.1,
+                             height_shift_range=0.1)
+# Creates our batch of one image
+pic = datagen.flow(img_tensor, batch_size=1)
+plt.figure(figsize=(16, 16))
+# Plots our figures
+for i in range(1, 17):
+    plt.subplot(4, 4, i)
+    batch = pic.next()
+    image_ = batch[0].astype('uint8')
+    plt.imshow(image_)
+plt.show()
