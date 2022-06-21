246f341a31a402f491018f787d4225a7f6252f3e
liang799 <71926348+liang799@users.noreply.github.com>
2022-06-21 15:18:33 +0800

    Updated path

--- ALU_app.py
+++ ALU_app.py
@@ -18,7 +18,7 @@ def import_and_predict(image_data, model):
     return prediction
 
 
-model = tf.keras.models.load_model('C:/Python/Apple-Lemon-Detector/tune.hdf5')  # loading a trained model
+model = tf.keras.models.load_model('tune.hdf5')  # loading a trained model
 
 st.write("""
          # Apple, Lemon, Unknown Detector
